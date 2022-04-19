8fd8971ea86b234ef878b93baf91e0b6c134dd03
Gamesmes90 <gamesmes90@gmail.com>
2022-04-19 20:44:31 +0200

    Fix License

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,21 @@
+/** 
+ * gitea-vscode-icons: shows vscode-icons on gitea repositories
+ * Copyright (C) 2022 Gamesmes90
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ * 
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
 import { getIconForFile, getIconForFolder, getIconForOpenFolder, DEFAULT_ROOT_OPENED } from 'vscode-icons-js';
 
 try{
